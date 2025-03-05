ae62ba2ee98f15ca8c970dd85aecf61ecc80b563
MrRayliegh <edwardcole143@gmail.com>
2025-03-05 11:50:55 +0800

    Update settings.py

--- Scheduling_load/Scheduling_load/settings.py
+++ Scheduling_load/Scheduling_load/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tocv8(jup586=68#v26py9_b)*-)l@wq6nl%o!=&y@0m6tyv8l"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
