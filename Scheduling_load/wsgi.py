5b90f19235d2e7f5eec5475ec4627a58bb5675fa
MrRayliegh <edwardcole143@gmail.com>
2025-03-05 22:42:26 +0800

    Update wsgi.py

--- Scheduling_load/wsgi.py
+++ Scheduling_load/wsgi.py
@@ -11,7 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Scheduling_load.settings')
-
 
 application = get_wsgi_application()
