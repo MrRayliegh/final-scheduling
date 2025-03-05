5b81fd2c2dfbeed8e53f29607b5cc986dfccbe8f
MrRayliegh <edwardcole143@gmail.com>
2025-03-05 22:43:37 +0800

    Update urls.py

--- Scheduling_load/urls.py
+++ Scheduling_load/urls.py
@@ -4,8 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('scheduling_system.urls')), 
-<<<<<<< HEAD:django project/Scheduling_load/urls.py
 ]
-=======
-]
->>>>>>> 968c981 (Initial commit with Django project):Scheduling_load/urls.py
