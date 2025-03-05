fd39f7c351f9e80680a66d530157120c5eb8bdd1
MrRayliegh <edwardcole143@gmail.com>
2025-03-05 20:17:09 +0800

    Delete Scheduling_load directory

--- Scheduling_load/Scheduling_load/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('ctu_scheduler/', include('scheduling_system.urls')),  # Base path for your app
-]
