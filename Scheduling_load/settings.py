b8b009e80d44c49a5ee5c700c37b175409dda665
MrRayliegh <edwardcole143@gmail.com>
2025-03-05 23:27:00 +0800

    Update settings.py

--- Scheduling_load/settings.py
+++ Scheduling_load/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'whitenoise.runserver_nostatic',
     'scheduling_system',
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "Scheduling_load.urls"
